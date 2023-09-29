b51f6c3f81b794b7f91f306754a5c5e07cafbdd3
Shereen3781 <110721883+Shereen3781@users.noreply.github.com>
2023-09-29 20:27:09 +0300

    Update creditriskapp.py

--- creditriskapp.py
+++ creditriskapp.py
@@ -26,8 +26,8 @@ st.markdown("This project is using data from kaggle about clients who get home l
 
 new_title = '<p style="font-family:sans-serif; color:Green; font-size: 30px;">Distribution of multiple features with TARGET</p>'
 st.markdown(new_title, unsafe_allow_html=True)
-row3_spacer1, row3_1, row3_spacer2 = st.columns((.2, 7.1, .2))
-with row3_1:
+row4_spacer1, row4_1, row4_spacer2 = st.columns((.2, 7.1, .2))
+with row4_1:
     option5 = st.selectbox('selsect an option' , ['AMT_ANNUITY','AMT_GOODS_PRICE','DAYS_EMPLOYED','DAYS_BIRTH', 'EXT_SOURCE_2', 'EXT_SOURCE_3','CNT_CHILDREN'],key = "tap_sec5")
     t1 = app_train.loc[app_train['TARGET'] != 0]
     t0 = app_train.loc[app_train['TARGET'] == 0]
@@ -37,7 +37,7 @@ with row3_1:
     plt.ylabel('Density plot', fontsize=12)
     plt.legend()
     st.pyplot(fig)
-with row3_2:
+with row5_1:
     st.markdown('some numerical features that seems to have relationship to the likelihood of an applicant to repay a loan are:')
     st.markdown('Loan annuity, the price of the goods for which the loan is given, period for which the client is employed,')
     st.markdown('normalized score from external data source, and number of children the client has')
