ff4f5a4b1619cb32cbeada1b82db03f94ad26f8e
Shereen3781 <110721883+Shereen3781@users.noreply.github.com>
2023-09-29 20:48:20 +0300

    Update creditriskapp.py

--- creditriskapp.py
+++ creditriskapp.py
@@ -38,6 +38,7 @@ with row4_1:
     plt.legend()
     st.pyplot(fig)
 with row4_2:
+    st.markdown('')
     st.markdown('Some numerical features that seems to have relationship to the likelihood of an applicant to repay a loan are:')
     st.markdown('Loan annuity,')
     st.markdown('The price of the goods for which the loan is given,')
