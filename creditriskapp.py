36e1010d753879598d615e2bcb7bddc4c0d1a3f2
Shereen3781 <110721883+Shereen3781@users.noreply.github.com>
2023-09-29 20:56:09 +0300

    Update creditriskapp.py

--- creditriskapp.py
+++ creditriskapp.py
@@ -18,11 +18,11 @@ with row0_2:
     image = Image.open('about-us-home-credit.jpg')
     st.image(image, width=300)
 
-st.markdown(":blue[This project is using data from kaggle about clients who get home loans.\
+st.markdown("This project is using data from kaggle about clients who get home loans.\
     It aims to use of a variety of data to predict clients' repayment abilities.\
     Doing so will ensure that clients capable of repayment are not rejected and that loans\
     are given with a principal, maturity, and repayment calendar that will empower clients\
-    to be successful.]")
+    to be successful.")
 
 new_title = '<p style="font-family:sans-serif; color:Green; font-size: 30px;">Distribution of multiple features with TARGET</p>'
 st.markdown(new_title, unsafe_allow_html=True)
@@ -41,12 +41,12 @@ with row4_2:
     st.markdown(' ')
     st.markdown(' ')
     st.markdown(' ')
-    st.markdown('Some numerical features that seems to have relationship to the likelihood of an applicant to repay a loan are:')
-    st.markdown('Loan annuity,')
-    st.markdown('The price of the goods for which the loan is given,')
-    st.markdown('Period for which the client is employed,')
-    st.markdown('Normalized score from external data source, and number of children the client has, and')
-    st.markdown('Number of children the client has')
+    st.markdown(':blue[Some numerical features that seems to have relationship to the likelihood of an applicant to repay a loan are:]')
+    st.markdown(':blue[Loan annuity,]')
+    st.markdown(':blue[The price of the goods for which the loan is given,]')
+    st.markdown(':blue[Period for which the client is employed,]')
+    st.markdown(':blue[Normalized score from external data source, and number of children the client has, and]')
+    st.markdown(':blue[Number of children the client has]')
 
 new_title = '<p style="font-family:sans-serif; color:Green; font-size: 30px;">Characteristics of people who not repay their loan</p>'
 st.markdown(new_title, unsafe_allow_html=True)
@@ -62,12 +62,12 @@ s.axhline(global_mean, linewidth=3, color='b')
 plt.text(0, global_mean - 0.01, "global_mean", color='black', weight='semibold')
 st.pyplot(fig)
 
-st.markdown("Revolving loans are just a small fraction from the total number of loans. But compared with their frequency, a larger amount of Revolving loans, are not repaid.")
-st.markdown("The number of female clients is almost double the number of male clients. But, males have a higher chance of not repaying their loans (almost 10%), comparing with women (almost 7%).")
-st.markdown("Client owns a car are almost a half of the ones that doesn't own one. But they have a lower chance of not repaying their loans than the ones that doesn't own a car.")
-st.markdown("Very few people are registered in not live. Generally, not repayment rate is slightly larger for these cases than in the rest.")
-st.markdown("Majority of the clients have Secondary / secondary special education, followed by clients with Higher education. Only a very small number having an academic degree.The Lower secondary category, although rare, have the largest rate of not returning the loan (11%).\
-The people with Academic degree have less than 2% not-repayment rate.")
+st.markdown(":blue[Revolving loans are just a small fraction from the total number of loans. But compared with their frequency, a larger amount of Revolving loans, are not repaid.]")
+st.markdown(":blue[The number of female clients is almost double the number of male clients. But, males have a higher chance of not repaying their loans (almost 10%), comparing with women (almost 7%).]")
+st.markdown(":blue[Client owns a car are almost a half of the ones that doesn't own one. But they have a lower chance of not repaying their loans than the ones that doesn't own a car.]")
+st.markdown(":blue[Very few people are registered in not live. Generally, not repayment rate is slightly larger for these cases than in the rest.]")
+st.markdown(":blue[Majority of the clients have Secondary / secondary special education, followed by clients with Higher education. Only a very small number having an academic degree.The Lower secondary category, although rare, have the largest rate of not returning the loan (11%).\
+The people with Academic degree have less than 2% not-repayment rate.]")
 
 option3 = st.selectbox('selsect an option' , ['ORGANIZATION_TYPE','NAME_INCOME_TYPE'],key = "tap_sec3")
 cat_perc=app_train[[option3, 'TARGET']].groupby([option3],as_index=False).mean().sort_values(by='TARGET', ascending=False)
@@ -82,9 +82,9 @@ plt.text(0, global_mean - 0.01, "global_mean", color='black', weight='semibold')
 fig.subplots_adjust(hspace =0.6)
 st.pyplot(fig)
 
-st.markdown("Oraganizations with highest percent of loans not repaid are Transport: type 3 (16%), Industry: type 13 (13.5%), Industry: type 8 (12.5%) and Restaurant (less than 12%).")
-st.markdown("Most of clients have income from Working, followed by Commercial associate, Pensioner and State servant.\
-Clients with maternity leave have not-repayment rates of almost 40%, followed by Unemployed (37%). The rest of types of incomes have not-repayment rates of 10%.")
+st.markdown(":blue[Oraganizations with highest percent of loans not repaid are Transport: type 3 (16%), Industry: type 13 (13.5%), Industry: type 8 (12.5%) and Restaurant (less than 12%).]")
+st.markdown(":blue[Most of clients have income from Working, followed by Commercial associate, Pensioner and State servant.\
+Clients with maternity leave have not-repayment rates of almost 40%, followed by Unemployed (37%). The rest of types of incomes have not-repayment rates of 10%.]")
 
 
 new_title = '<p style="font-family:sans-serif; color:Green; font-size: 30px;">Missing values statistics</p>'
@@ -100,8 +100,8 @@ plt.xlabel('Features', fontsize=15)
 plt.ylabel('Percent of missing values', fontsize=15)
 plt.title('Percent missing data by feature', fontsize=15)
 st.pyplot(fig)
-st.markdown("It appears there are many applicants who leave blank the information for their housing.\
-Can use the missing data as a feature itself by counting missing values bfeore droping them")
+st.markdown(":blue[It appears there are many applicants who leave blank the information for their housing.\
+Can use the missing data as a feature itself by counting missing values bfeore droping them.]")
 
 new_title = '<p style="font-family:sans-serif; color:Green; font-size: 30px;">Most correlated features</p>'
 st.markdown(new_title, unsafe_allow_html=True)
